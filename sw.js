d0f7ac6b15cfccbaab6f5d8617fb90f3d893db5c
Rex <ruinazuo@gmail.com>
2026-08-17 06:07:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-const CACHE_NAME = 'xiaoshuji-pwa-v1';
-const APP_SHELL = ['./', './index.html', './manifest.json'];
+const CACHE = "xiaoshuji-pwa-v2";
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
